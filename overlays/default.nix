acdf29cc9aa7fb258a15134f2a965cfd613c7f3a
luqmanishere <luqmanulhakim1720@gmail.com>
2023-03-07 23:31:46 +0800

    overlay: formatting

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,9 +15,7 @@
     # waybar with Hyprland IPC support, allow workspace switching
     waybar = prev.waybar.overrideAttrs (oldAttrs: rec {
       mesonFlags = (oldAttrs.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
-      preConfigurePhases = [
-        "hyprPhase"
-      ] ++ (oldAttrs.preConfigurePhase or [ ]);
+      preConfigurePhases = (oldAttrs.preConfigurePhase or [ ]) ++ [ "hyprPhase" ];
       hyprPhase = ''
         sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
       '';
