90df18736b3b7ea94dc4130706ae6679a0cdea01
luqmanishere <luqmanulhakim1720@gmail.com>
2023-03-12 16:39:40 +0800

    asuna: limit boot configurations

--- nixos/asuna.nix
+++ nixos/asuna.nix
@@ -13,6 +13,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   networking.hostName = "asuna"; # Define your hostname.
