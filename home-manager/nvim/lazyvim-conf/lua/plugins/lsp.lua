57df25c3052443dfeca6406f6f09149de908d5b6
luqmanishere <luqmanulhakim1720@gmail.com>
2023-06-07 06:09:58 +0800

    nvim: add keybind for hover actions

--- home-manager/nvim/lazyvim-conf/lua/plugins/lsp.lua
+++ home-manager/nvim/lazyvim-conf/lua/plugins/lsp.lua
@@ -40,6 +40,11 @@ return {
   -- add wanted servers here
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      keys[#keys + 1] = { "<leader>ch", vim.lsp.buf.hover, desc = "Hover Action" }
+    end,
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
--- home-manager/nvim/lazyvim-conf/lua/plugins/rust.lua
+++ home-manager/nvim/lazyvim-conf/lua/plugins/rust.lua
@@ -1,11 +1,15 @@
 return {
 
   -- allow rust-tools to manage rust-analyzer
-  { 'simrat39/rust-tools.nvim',
-    config = true },
+  {
+    'simrat39/rust-tools.nvim',
+    config = true,
+  },
 
-  { 'saecki/crates.nvim',
-    config = true },
+  {
+    'saecki/crates.nvim',
+    config = true
+  },
 
   -- { "hrsh7th/nvim-cmp",
   --   opts = {
