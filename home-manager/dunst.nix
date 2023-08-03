209732397a03a5ebb4220383958eefcb51eb5f99
luqmanishere <luqmanulhakim1720@gmail.com>
2023-08-03 12:34:44 +0800

    dunst: wip attempt at cleaner notifications

--- home-manager/dunst.nix
+++ home-manager/dunst.nix
@@ -29,36 +29,47 @@ in {
         global = {
           monitor = 0;
           follow = "mouse";
-          origin = "top-center";
           idle-threshold = 30;
           dmenu = "${pkgs.rofi}/bin/rofi -dmenu -i -p ''";
           layer = "overlay";
+          notification_limit = 5;
+          progress_bar = true;
+
+          origin = "top-right";
+          width = "(0,500)";
+          height = 150;
+          offset = "80x30";
 
           mouse_left_click = "do_action";
           mouse_right_click = "context";
+          mouse_middle_click = "close_all";
 
           corner-radius = 10;
           frame_color = "#89B4FA";
           separator_color = "frame";
-          max_icon_size = 128;
-          min_icon_size = 128;
+          max_icon_size = 100;
+          min_icon_size = 100;
+          gap_size = 2;
           font = "Noto Sans Display Nerd Font 12";
         };
 
         urgency_low = {
           background = "#1E1E2E";
           foreground = "#CDD6F4";
+          timeout = 5;
         };
 
         urgency_normal = {
           background = "#1E1E2E";
           foreground = "#CDD6F4";
+          timeout = 5;
         };
 
         urgency_critical = {
           background = "#1E1E2E";
           foreground = "#CDD6F4";
           frame_color = "#FAB387";
+          timeout = 5;
         };
       };
     };
