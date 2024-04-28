2bb722cc9642033a5c1b655af81e42199321e6fe
luqmanishere <luqmanulhakim1720@gmail.com>
2024-04-28 14:36:24 +0800

    fix devenv action

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -38,9 +38,9 @@ in {
 
       auto-optimise-store = true;
 
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-      trusted-substituters = ["https://hyprland.cachix.org"];
+      # substituters = ["https://hyprland.cachix.org"];
+      # trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      # trusted-substituters = ["https://hyprland.cachix.org"];
     };
   };
 }
