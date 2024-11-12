0a325c8bf3cec0547900d5b40ea7e6c6c0ed919b
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2024-11-12 22:12:36 +0800

    migrate others attempt #1

--- configurations/nixos/asuna/hm.nix
+++ configurations/nixos/asuna/hm.nix
@@ -2,7 +2,7 @@
   inherit (flake) inputs;
   inherit (inputs) self;
 in {
-  # Enable home-manager for "runner" user
+  # Enable home-manager for "luqman" user
   home-manager.users."luqman" = {
     imports = [(self + /configurations/home/luqman.nix)];
   };
