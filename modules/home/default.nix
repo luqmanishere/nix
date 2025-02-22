e1f9b83e519ed00e1609ea264d4ddcb3460c221c
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2025-02-22 21:22:15 +0800

    mpd should only be on desktop usage

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -15,7 +15,7 @@ in {
     ./all/tools/shell.nix
     ./all/tools/starship.nix
     ./all/terminals/zellij.nix
-    ./all/tools/mpd.nix
+    # ./all/tools/mpd.nix
     # ./all/tools/task.nix
     # ./all/secrets
     # ./all/tools/python.nix
