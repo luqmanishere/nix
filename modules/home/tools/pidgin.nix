b648210372b9c823585de4001785acdf1ccb754f
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2025-03-19 23:12:53 +0800

    fix pidgin module being unguarded

--- modules/home/tools/pidgin.nix
+++ modules/home/tools/pidgin.nix
@@ -1,8 +1,25 @@
-{pkgs, ...}: {
-  config = {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.modules.tools.pidgin;
+in {
+  options.modules.tools.pidgin = {enable = mkEnableOption "Enable the pidgin messenger app";};
+
+  config = mkIf cfg.enable {
     programs.pidgin = {
       enable = true;
       plugins = [];
     };
+
+    assertions = [
+      {
+        assertion = config.modules.core.gui.enable;
+        message = "a graphical environment is required";
+      }
+    ];
   };
 }
