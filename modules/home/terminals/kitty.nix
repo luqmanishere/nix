bb52e281721f37b6b96e03cdc472616717f7ac45
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2025-05-19 19:51:46 +0800

    some keybinds for kitty

--- modules/home/terminals/kitty.nix
+++ modules/home/terminals/kitty.nix
@@ -40,6 +40,14 @@ in {
       extraConfig = mkMerge [
         ''
           macos_option_as_alt yes
+          map alt+1 goto_tab 1
+          map alt+2 goto_tab 2
+          map alt+3 goto_tab 3
+          map alt+4 goto_tab 4
+          map alt+5 goto_tab 5
+          map alt+shift+\ launch --location vsplit
+          map alt+- launch --location hsplit
+          map alt+shift+c new_tab --cwd
         ''
         (mkIf cfg.mapleFont ''
           font_features MapleMonoNF-Regular +calt -zero -cv01 -cv02 -cv03 -cv04 ss01 -ss02 ss03
