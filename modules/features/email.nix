c9cd8d5f8aa3a67b4f43b352f2c1ab0898a6634c
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2026-04-04 00:37:13 +0800

    email: full sync config
    add afew, then put all programs in notmuch hooks, set to run every 5
    minutes using systemd service and timer

--- modules/features/email.nix
+++ modules/features/email.nix
@@ -1,10 +1,26 @@
 {...}: {
-  flake.modules.homeManager.email = {...}: {
+  flake.modules.homeManager.email = {pkgs, ...}: {
     programs.mbsync.enable = true;
     programs.msmtp.enable = true;
     programs.notmuch = {
       enable = true;
-      hooks = {preNew = "mbsync --all";};
+      hooks = {
+        preNew = "${pkgs.isync}/bin/mbsync --all";
+        postNew = "${pkgs.afew}/bin/afew -tn";
+      };
+      new.tags = ["new"];
+    };
+    programs.afew = {
+      enable = true;
+      extraConfig = ''
+        [SpamFilter]
+        [KillThreadsFilter]
+        [SentMailsFilter]
+        sent_tag = sent
+        [ArchiveSentMailsFilter]
+
+        [InboxFilter]
+      '';
     };
 
     accounts.email = {
@@ -25,6 +41,32 @@
         };
         userName = "luqmanulhakim1720@gmail.com";
         passwordCommand = "pass show email/main-mbsync";
+      };
+    };
+
+    systemd.user = {
+      services.email-sync = {
+        Unit = {
+          Description = "Email sync";
+        };
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.notmuch}/bin/notmuch new";
+        };
+      };
+      timers.email-sync = {
+        Unit = {
+          Description = "Timer triggering email-sync";
+        };
+
+        Timer = {
+          OnBootSec = "1min";
+          OnCalendar = "*:0/05";
+          Persistent = true;
+          Unit = "email-sync.service";
+        };
+
+        Install.WantedBy = ["timers.target"];
       };
     };
   };
