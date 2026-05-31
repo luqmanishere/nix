801e41867211145432c3fc5813b91c4b555a1aab
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2026-05-31 20:47:04 +0800

    add fontconfig to system side too

--- modules/features/fonts.nix
+++ modules/features/fonts.nix
@@ -1,4 +1,30 @@
 {...}: {
+  flake.modules.nixos.fonts = {pkgs, ...}: {
+    fonts.fontconfig.enable = true;
+    environment.systemPackages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      jetbrains-mono
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.noto
+      nerd-fonts.symbols-only
+      material-symbols
+      dejavu_fonts
+      # TODO: fix packages
+      # self.packages.${pkgs.system}.iosevka-solemnattic
+
+      maple-mono.NF
+      maple-mono.Normal-NF
+
+      fontconfig
+    ];
+  };
+
   flake.modules.homeManager.fonts = {pkgs, ...}: {
     home.packages = with pkgs; [
       # fonts
