eff33622b12e4ecfe18f1d55d9d5f35d97c846ab
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2026-06-01 23:29:01 +0800

    use limine for asuna bootloader with windows entry

--- modules/hosts/asuna/bootloader.nix
+++ modules/hosts/asuna/bootloader.nix
@@ -1,9 +1,20 @@
 {...}: {
-  flake.modules.nixos.asuna-boot = {
-    boot.loader.systemd-boot = {
-      enable = true;
-      configurationLimit = 5;
-    };
+  flake.modules.nixos.asuna-boot = {pkgs, ...}: {
+    # boot.loader.systemd-boot = {
+    #   enable = true;
+    #   configurationLimit = 5;
+    # };
+    environment.systemPackages = with pkgs; [sbctl];
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.limine = {
+      enable = true;
+      maxGenerations = 5;
+      secureBoot = {enable = true;};
+      extraEntries = ''
+        /Windows
+          protocol:efi
+          path: uuid(7df2b372-d548-41ad-853e-3c2f155c7170):/EFI/Microsoft/Boot/bootmgfw.efi
+      '';
+    };
   };
 }
