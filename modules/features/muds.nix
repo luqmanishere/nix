fbb40cdd17b7da5afe4a66360b11c69acb495f22
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2026-09-02 21:43:53 +0800

    fix(muds): drop blightmud package

--- modules/features/muds.nix
+++ modules/features/muds.nix
@@ -8,7 +8,6 @@
     home.packages = with pkgs; [
       # mudlet-custom
       mudforge-appimage
-      inputs.blightmud.packages.${pkgs.stdenv.hostPlatform.system}.blightmud-tts
       rune-mud
     ] ++ lib.optionals config.modules.core.gui.enable [
       # GUI-only; only install where a graphical session exists
